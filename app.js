704efce08fe77cc228ac024494afc26eda155d73
sahal2255 <sahalvv9656@gmail.com>
2025-01-29 11:17:15 +0530

    route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express=require('express')
 require('dotenv').config()
 const app=express()
 
+
+
+app.get('/',(req,res)=>{
+    res.send('checking route')
+})
 app.listen(process.env.PORT,()=>{
     console.log('server running')
 })
